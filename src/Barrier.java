f6fa7a4bc40057cdd7cee089feeead8301b63f0d
Thomas <s153047@student.dtu.dk>
2017-11-16 18:42:28 +0100

    fixede barrier
    barrierSem renamed til syncSem og indført en ny barrierSem(9)

--- src/Barrier.java
+++ src/Barrier.java
@@ -3,15 +3,18 @@ public class Barrier {
 
 	Semaphore barrierSem;
 	Semaphore counterSem;
+	Semaphore gateSem;
 	boolean isBarrierOn;
 	int barrierCounter;
 	public Barrier () {
 		barrierSem = new Semaphore(0);
 		counterSem = new Semaphore(1);
+		gateSem = new Semaphore(9);
 		isBarrierOn = false;
 		barrierCounter = 0;
 	}
 	public void sync() throws InterruptedException {
+		gateSem.P();
 		counterSem.P();
 		barrierCounter++;
 		if(barrierCounter == 9) {
@@ -27,6 +30,7 @@ public class Barrier {
 		barrierSem.V();
 		if(barrierCounter == 0) {
 			barrierSem.P();
+			for(int i = 0; i<9;i++) gateSem.V();
 		}
 		counterSem.V();
 	}
