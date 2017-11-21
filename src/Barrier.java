0880fdbfbfa66a1a717ef20a2c4bb23f0762a163
Thomas <s153047@student.dtu.dk>
2017-11-21 21:24:17 +0100

    fixede monitor barrier

--- src/Barrier.java
+++ src/Barrier.java
@@ -2,41 +2,65 @@
 public class Barrier {
 	boolean isBarrierOn;
 	boolean shutdown;
-	boolean release;
-	int barrierCounter;
+	boolean release1;
+	boolean release2;
+	int barrierCounter1;
+	int barrierCounter2;
 	int amountOfCars;
 	public Barrier () {
 		isBarrierOn = false;
-		release = false;
-		barrierCounter = 0;
+		release1 = false;
+		release2 = false;
+		barrierCounter1 = 0;
+		barrierCounter2 = 0;
 		amountOfCars = 9;
 
 	}
 	public synchronized void sync(Semaphore removeCarSem) throws InterruptedException {
-		barrierCounter++;
+		barrierCounter1++;
+		
 		removeCarSem.V();
-		while(barrierCounter != amountOfCars && !release) {
+		while(barrierCounter1 != amountOfCars && !release1) {
 			wait();
 		}
 		removeCarSem.P();
-		if(barrierCounter == amountOfCars) {
-			release = true;
+		if(barrierCounter1 == amountOfCars) {
+			release1 = true;
 			notifyAll();
 			if(shutdown) {
 				isBarrierOn = false;
 			}
 		} 
-		
-		barrierCounter--;
-		if(barrierCounter == 0) {
-			release = false;
+		barrierCounter1--;
+		if(barrierCounter1 == 0) {
+			release1 = false;
 		}
+		
+		barrierCounter2++;
+		removeCarSem.V();
+		while(barrierCounter2 != amountOfCars && !release2) {
+			wait();
+		}
+		removeCarSem.P();
+		if(barrierCounter2 == amountOfCars) {
+			release2 = true;
+			notifyAll();
+			if(shutdown) {
+				isBarrierOn = false;
+			}
+		} 
+		barrierCounter2--;
+		if(barrierCounter2 == 0) {
+			release2 = false;
+		}
+		
 	}
 	
 	public synchronized void on() {
 		if(!isBarrierOn) {
 			isBarrierOn = true;
-			release = false;
+			release1 = false;
+			release2 = false;
 		}
 		shutdown = false;
 	}
@@ -44,7 +68,8 @@ public class Barrier {
 	public synchronized void off() {
 		if(isBarrierOn) {
 			isBarrierOn = false;
-			release = true;
+			release1 = true;
+			release2 = true;
 			notifyAll();
 		}
 	}
@@ -55,7 +80,8 @@ public class Barrier {
 	}
 	public synchronized void removeCar(boolean atBarrier) {
 		if(atBarrier && isBarrierOn) {
-			barrierCounter--;
+			barrierCounter1--;
+			barrierCounter2--;
 		}
 		amountOfCars--;
 		notifyAll();
